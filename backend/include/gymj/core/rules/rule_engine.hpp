907299511698e628afebcd52ff04c0311f741ddb
aimyon <1994664200@qq.com>
2026-08-11 20:57:40 +0800

    2026/08/11 rule_config.hpp

--- backend/include/gymj/core/rules/rule_engine.hpp
+++ backend/include/gymj/core/rules/rule_engine.hpp
@@ -1,16 +1,27 @@
 #ifndef GYMJ_CORE_RULE_ENGINE_HPP
 #define GYMJ_CORE_RULE_ENGINE_HPP
 
+#include <vector>
+#include <array>
+
 #include <gymj/core/point/point_engine.hpp>
 #include <gymj/core/rules/game_engine.hpp>
+#include <gymj/common/player/player_action.hpp>
 
 namespace gymj::rule{
+
+using gymj::common::PlayerAction;
+using gymj::common::RoundState;
+using gymj::common::PointRuleConfig;
+using gymj::common::GameRuleConfig;
 
 class RuleEngine{
 public:
     RuleEngine();
-
+    std::array<std::array<PlayerAction, 4>, 4> get_available_actions(const RoundState& state);
 private:
+    PointEngine point_engine_;
+    GameEngine game_engine_;
 };
 
 }
